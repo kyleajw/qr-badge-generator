d8a2b74a76a3da475b4c5d59670bf7807504cff2
Kyle Whelan <kyle.aj.whelan@outlook.com>
2022-10-03 23:03:27 +0100

    ADDED QR GENERATION

--- app.js
+++
@@ -1,7 +0,0 @@
-import qrcode from './node_modules/qrcode-generator/qrcode';
-
-qr = qrcode(4, 'L');
-qr.addData('Hi');
-qr.make();
-
-document.getElementById('placeHolder').innerHTML = qr.createImgTag();
---
+++ src/index.js
@@ -0,0 +1,35 @@
+let qrcode = require('../node_modules/qrcode-generator/qrcode.js');
+
+let typeNumber = 12;
+let errorCorrectionLevel = 'L';
+let qr = qrcode(typeNumber, errorCorrectionLevel);
+
+
+
+document.querySelector('.submit-info').addEventListener('click', function (e) {
+    e.preventDefault();
+    createQRCode();
+})
+
+function createQRCode() {
+    const userInfo = {
+        firstName: document.querySelector('#first-name').value,
+        lastName: document.querySelector('#last-name').value,
+        email: document.querySelector('#email').value,
+        twitter: document.querySelector('#twitter').value,
+        github: document.querySelector('#github').value
+    }
+    console.log(userInfo);
+    if (userInfo.firstName === '' || userInfo.lastName === '' || userInfo.email === '') {
+
+    } else {
+        qr.addData(`First Name: ${userInfo.firstName}\nLast Name: ${userInfo.lastName}\nEmail: ${userInfo.email}\nTwitter: ${userInfo.twitter}\nGithub: ${userInfo.github}`);
+        qr.make();
+        document.getElementById('placeHolder').innerHTML = qr.createImgTag();
+    }
+}
+
+
+
+
+

a9163bb18be7f0a47a6df34d26ca479994cb764b
Kyle Whelan <kyle.aj.whelan@outlook.com>
2022-10-04 16:21:26 +0100

    removes submit form and displays user details w/qr

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 let qrcode = require('../node_modules/qrcode-generator/qrcode.js');
 
-let typeNumber = 12;
+let typeNumber = 24;
 let errorCorrectionLevel = 'L';
 
 
+let qr = qrcode(typeNumber, errorCorrectionLevel);
 
 
 document.querySelector('.submit-info').addEventListener('click', function (e) {
     e.preventDefault();
+
     createQRCode();
+
 })
 
 function createQRCode() {
@@ -23,14 +26,33 @@ function createQRCode() {
     if (userInfo.firstName === '' || userInfo.lastName === '' || userInfo.email === '') {
 
     } else {
-        let qr = qrcode(typeNumber, errorCorrectionLevel);
-        qr.addData(`First Name: ${userInfo.firstName}\nLast Name: ${userInfo.lastName}\nEmail: ${userInfo.email}\nTwitter: ${userInfo.twitter}\nGithub: ${userInfo.github}`);
-        qr.make();
-        document.getElementById('placeHolder').innerHTML = qr.createImgTag();
+
+        displayBadge(userInfo);
     }
 }
 
+function displayBadge(userData) {
+    document.querySelector('.user-info').remove();
+    qr.addData(`First Name: ${userData.firstName}\nLast Name: ${userData.lastName}\nEmail: ${userData.email}`);
 
+    document.querySelector('.badge').innerHTML = `<h1 id="badge-full-name">${userData.firstName} ${userData.lastName}</h1>
+    <h2 id="badge-email">${userData.email}</h2>`;
+    if (userData.twitter != '') {
+        document.querySelector('.badge').innerHTML += `<h2 id="badge-twitter">${userData.twitter}</h2>`;
+        qr.addData(`\nTwitter: ${userData.twitter}`);
+    }
+    if (userData.github != '') {
+        document.querySelector('.badge').innerHTML += `<h2 id="badge-github">${userData.github}</h2>`;
+        qr.addData(`\nGithub: ${userData.github}`);
+    }
+    document.querySelector('.badge').innerHTML += `<div id="qr-placeholder"></div>`;
+    document.querySelector('.badge').innerHTML += `<div id="bottom-block"></div>`;
+
+    qr.make();
+    document.getElementById('qr-placeholder').innerHTML = qr.createImgTag();
+
+
+}
 
 
 
